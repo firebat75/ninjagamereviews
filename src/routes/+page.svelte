b32a699e7c82a125bc8cacd9d833a3f9a27c7f6e
firebat75 <jacklowry1337@gmail.com>
2022-09-23 09:30:29 -0400

    changed +page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let title = "Ninja Gaming Guides";
+
+    const updateTitle = () => {
+        title = "something completely different";
+    };
+</script>
+
+<div class="index">
+    <h2>{title}</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+    </p>
+
+    <button on:click={updateTitle}>change title</button>
+    <input type="text" bind:value={title} />
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
